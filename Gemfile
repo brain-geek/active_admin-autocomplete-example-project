70f694e8aae21e43f449ec249edaf689f794bb43
Alex Rozumey <brain-geek@yandex.ua>
2012-03-12 22:28:49 +0200

    Remove pry from example

--- Gemfile
+++ Gemfile
@@ -7,8 +7,6 @@ gem 'rails', '3.2.2'
 
 gem 'sqlite3'
 
-gem 'pry'
-
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
--- app/admin/user_comments.rb
+++ app/admin/user_comments.rb
@@ -3,7 +3,6 @@ ActiveAdmin.register UserComment do
     f.inputs "Details" do
       f.input :article, :as => :string, :input_html => { :class => 'article_text', :name => '' }
       f.input :article_id, :as => :hidden, :input_html => { :class => 'article_id' }
-      #binding.pry
     end
 
     f.buttons
