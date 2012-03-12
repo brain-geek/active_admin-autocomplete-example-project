d8f127e7c7113f6847b68d38135fe197f5bae9ea
Alex Rozumey <brain-geek@yandex.ua>
2012-03-12 22:24:59 +0200

    Add js and css as final step

--- app/admin/user_comments.rb
+++ app/admin/user_comments.rb
@@ -1,3 +1,11 @@
 ActiveAdmin.register UserComment do
-  
+  form do |f|
+    f.inputs "Details" do
+      f.input :article, :as => :string, :input_html => { :class => 'article_text', :name => '' }
+      f.input :article_id, :as => :hidden, :input_html => { :class => 'article_id' }
+      #binding.pry
+    end
+
+    f.buttons
+  end  
 end
--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -1,1 +1,33 @@
 //= require active_admin/base
+
+ $(function(){  
+  $('.article_text').autocomplete({  
+    minLength: 3,  
+    delay: 600,  
+    source: function(request, response) {  
+      $.ajax({  
+        //Url to page with json
+        url: "/admin/articles",  
+        dataType: "json",  
+        data: {  
+          //Here you can pass any meta_search valid conditions
+         'q[translations_title_contains]': request.term  
+        },  
+        success: function( data ) {  
+          response( data );  
+        }  
+      });  
+    },  
+    select: function( event, ui ) {  
+     $( ".article_text" ).val( ui.item.title );  
+     $( ".article_id" ).val( ui.item.id );  
+     return false;  
+    }  
+   })  
+   .data( "autocomplete" )._renderItem = function( ul, item ) {  
+    return $( "<li></li>" )  
+     .data( "item.autocomplete", item )  
+     .append( "<a>" + item.title + "</a>" )  
+     .appendTo( ul );  
+   };  
+ });  
